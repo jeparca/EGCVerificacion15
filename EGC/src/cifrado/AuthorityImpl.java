eb205ca185114cf71547fa2fcd4de0f317eaa954
AlbertoGarciaSanchez <albgarsanuni@yahoo.es>
2015-11-26 13:09:03 +0100

    Comentado el método postKey() de la clase AuthorityImpl. Se encuentra la información de parámetros utilizados (entrada, salida) y del uso de una librería externa.

--- EGC/src/cifrado/AuthorityImpl.java
+++ EGC/src/cifrado/AuthorityImpl.java
@@ -8,6 +8,13 @@ import main.java.RemoteDataBaseManager;
 
 public class AuthorityImpl implements Authority{
 	
+	/**
+	 * Esta función obtiene las claves pública y privada de la votación cuyo id es el pasado 
+	 * como parámetro. Resaltar que hacemos uso del proyecto Elliptic_SDK, que  es una librería
+	 * criptográfica elíptica bajo la licensia GPL v3. Más información en la clase CryptoEngine.java
+	 * @param id. Corresponde al id de la votación
+	 * @return res. Boolean que indica si la operación ha tenido éxito.
+	 */
 	public boolean postKey(String id) {
 		boolean res;
 		BigInteger secretKey;
