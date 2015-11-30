2f2a7014445ce90d0555714cb053d9a867af6d7e
Jesús Pardo <jeparca_94@hotmail.com>
2015-11-30 16:46:54 +0100

    Incluido metodo de escritura de claves
    Se aÃade metodo para la escritura de claves en el proceso de
    refactorizacion del codigo PHP

--- HostingFiles/ScriptsPhp/keysManagement.php
+++ HostingFiles/ScriptsPhp/keysManagement.php
@@ -19,4 +19,25 @@ function getBothKeys($idVotation){
 	}
 }
 
+function writeKeys($idVotation){
+		
+	try{
+
+ 		$con = getConnector();
+ 	
+		$query = "INSERT INTO keysvotes (idvotation, publicKey, privateKey) VALUES (:id,:public,:private)";
+		$sentencia = $con->prepare($query);
+		$sentencia->bindParam(':id',$idVotation);
+		$sentencia->bindParam(':public',$publicKey);
+		$sentencia->bindParam(':private',$privateKey);
+
+		$insert = $sentencia->execute();
+		$con=null;
+
+		return $insert;
+ 	}catch(PDOException $e){
+		echo "Error: ".$e->GetMessage();
+	}
+}
+
 ?>
