379811e14a37f440527f6a79d550e890749a5570
Jesús Pardo <jeparca_94@hotmail.com>
2015-11-30 17:24:41 +0100

    Documentacion PHP del archivo de escritura de claves
    Se documenta el archivo de escritura de claves en la base de datos

--- HostingFiles/ScriptsPhp/writeKeys.php
+++ HostingFiles/ScriptsPhp/writeKeys.php
@@ -1,7 +1,15 @@
 <?php
 header('Content-Type: text/html; charset=utf-8');
 
+/***
+*** Incluimos el fichero donde se encuentran las funciones de lectura y escritura
+***/
 include_once('keysManagement.php');
+
+/***
+*** Se lee desde la URL el id de la votación, la clave pública y la clave privada
+*** que se quiere almacenar en la base de datos.
+***/
 if (isset($_REQUEST['id']) & isset($_REQUEST['public']) & isset($_REQUEST['private'])){
 	$votation_id = $_REQUEST['id'];
 	$publicKey = $_REQUEST['public'];
@@ -16,14 +24,24 @@ if (isset($_REQUEST['id']) & isset($_REQUEST['public']) & isset($_REQUEST['priva
 
 <?php
 
+	/***
+	*** Hacemos la operación de escritura
+	***/
 	$operation = writeKeys($votation_id, $publicKey, $privateKey);
 
+	/***
+	*** Mostramos un mensaje de operación completada con éxito si ha sido así y, sino un error.
+	***/
 	if($operation){
 		echo "Operation was completed successfully.";
 	}else{
 		echo "Ooops! An error happened while we was trying to write keys in database.";
 	}
-
+	
+/***
+*** Si no se ha proporcionado un id de votación, clave pública y clave privada en la URL o esta no sigue 
+*** el formato indicado, se muestra un error.
+***/
 }else{
 	echo "Votation id, public and private keys are mandatory. Please, check the URL format: http://egc.jeparca.com/writeKeys.php?id=XXXXXXXXXX&public=YYYYYYYYYY&private=ZZZZZZZZZZ";
 }
