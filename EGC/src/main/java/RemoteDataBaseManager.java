d80938f31dcf0723c97c34bf12cda61b2c9b8b89
Jesús Pardo <jeparca_94@hotmail.com>
2015-12-06 13:48:36 +0100

    Eliminados metodos de codigo heredado en desuso
    Se han eliminado todos los metodos de codigo heredados que ya no se usan
    debido al cambio de cifrado y nueva implementacion de acceso a la base de
    datos

--- EGC/src/main/java/RemoteDataBaseManager.java
+++ EGC/src/main/java/RemoteDataBaseManager.java
@@ -74,99 +74,7 @@ public class RemoteDataBaseManager {
 		
 		return success;
 	}
-	/**
-	 * Función que almacena en la base de datos una clave de cifrado que se usará
-	 * con el algoritmo AES.
-	 * @param id La id de la votación asociada a la clave de cifrado
-	 * @param secretKey Clave de cifrado a almacenar
-	 * @return Una variable booleana que indica el éxito o fracaso de la operación
-	 */
-	public boolean postAESKey(String id, String secretKey){
-		boolean success = false;
-		try {
-			
-			//Codificamos las variables que se enviarán en "UTF-8"
-			id = URLEncoder.encode(id, "UTF-8");
-			secretKey = URLEncoder.encode(secretKey, "UTF-8");
-	        URL url;
-			
-	        //URL que atenderá la petición HTTP y guardará la clave
-	        // en la base de datos remota
-			url = new URL("http://egc.jeparca.com/AESdefault2.php");
-			
-	        URLConnection connection = url.openConnection();
-	        connection.setDoOutput(true);
 	
-	        OutputStreamWriter out = new OutputStreamWriter(connection.getOutputStream());
-	        
-	        //Escribimos los valores en las variables para la petición HTTP
-	        out.write("id=" + id+"&");
-	        out.write("secretKey=" + secretKey);
-	        out.close();
-	
-	        //Obtenemos la respuesta de la petición
-	        BufferedReader in = new BufferedReader(new InputStreamReader( connection.getInputStream()));
-	        String decodedString;
-	        String fullText="";
-	        while ((decodedString = in.readLine()) != null) {
-	        	fullText+=decodedString;
-	        }
-	        in.close();
-	        //Comprobamos que en la respuesta está contenido el mensaje de éxito
-	        success = fullText.contains("New record created successfully");
-	        
-		} catch (MalformedURLException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		
-		return success;
-	}
-	/**
-	 * Función para leer los valores de las claves de cifrado asociadas a una votación.
-	 * @param id La id de la votación cuyas claves queremos consultar
-	 * @param method Variable que indica si se quieren consultar las claves RSA o la clave AES
-	 * @return Cadena de texto con el resultado de la petición HTTP
-	 */
-	public String readPage(String id){
-		BufferedReader in = null;
-		URL url = null;
-		String linea;
-		String textoPagina="";
-
-		//Hacemos una petición HTTP a una URL cuyo resultado podrá ser analizado
-		// posteriormente para extraer las claves RSA o la clave AES
-		try{
-			
-			url = new URL("http://egc.jeparca.com/AESdefault.php?id="+id);
-			
-		}catch (MalformedURLException e){
-			
-			e.printStackTrace();
-		}
-		
-				
-		try{
-			
-			in = new BufferedReader(new InputStreamReader(url.openStream()));
-		}catch(IOException e){
-			
-			e.printStackTrace();
-		}
-		//Guardamos en la variable 'textoPagina' el resultado de la petición HTTP	
-		try{
-			while ((linea = in.readLine()) != null) {
-			     textoPagina = textoPagina + linea;
-			}
-		}catch(IOException e){
-		
-			e.printStackTrace();
-		}
-		
-		
-		return textoPagina;
-	}
 	/**
 	 * Función usada para obtener la clave de cifrado AES asociada a una votación.
 	 * @param id La id de la votación cuya clave de cifrado AES queremos conocer
@@ -237,25 +145,6 @@ public class RemoteDataBaseManager {
 	    }
 	    
 	    return res;
-	}
-	
-	/**
-	 * Función usada para obtener la clave privada RSA asociada a una votación.
-	 * @param id La id de la votación cuya clave privada RSA queremos conocer
-	 * @return La clave privada asociada a una votación
-	 */
-	public String getPrivateKey(String id){
-		String fullPage = readPage(id);
-		String res = "";
-		
-		//En el bucle se extrae el valor de la clave analizando el resultado de llamar a la
-		// función readPage.
-		for(int j = fullPage.indexOf("Privatekey: ") + 11; fullPage.charAt(j)!='<' && j< fullPage.length() ;j++){
-			
-			res += fullPage.charAt(j);
-		}
-		
-		return res;
 	}
 
 	public static boolean sendGeneratedToken(Integer votationId, Integer token){
